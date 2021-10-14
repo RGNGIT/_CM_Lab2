ef95f4049327db2adbca7f118a10e211b808342f
БЕЗДОТНая Хоопс ✶ ДоскаИгры <vnifedow@bk.ru>
2021-10-14 18:00:53 +0500

    Добавьте файлы проекта.

---
+++ _(C++)CM_Lab2/gausslib.h
@@ -0,0 +1,98 @@
+#include <cmath>
+#define N 4
+
+double ** dptrSolvedMatrix;
+
+double ** copyMatrix(double ** dptrMatrix) {
+	double ** clone = new double * [N];
+	for (int i = 0; i < N; i++) {
+		clone[i] = new double[N];
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			clone[i][j] = dptrMatrix[i][j];
+		}
+	}
+	return clone;
+}
+
+double* copyVector(double * ptrVector) {
+	double * clone = new double[N];
+	for (int i = 0; i < N; i++) {
+		clone[i] = ptrVector[i];
+	}
+	return clone;
+}
+
+double * gaussSolve(double ** dptrMatrix, double * ptrEqual) {
+	double * x = new double[N];
+	double R;
+	for (int q = 0; q < N; q++) {
+		R = 1 / dptrMatrix[q][q];
+		dptrMatrix[q][q] = 1;
+		for (int j = q + 1; j < N; j++) {
+			dptrMatrix[q][j] *= R;
+		}
+		ptrEqual[q] *= R;
+		for (int k = q + 1; k < N; k++) {
+			R = dptrMatrix[k][q];
+			dptrMatrix[k][q] = 0;
+			for (int j = q + 1; j < N; j++) {
+				dptrMatrix[k][j] = dptrMatrix[k][j] - dptrMatrix[q][j] * R;
+			}
+			ptrEqual[k] = ptrEqual[k] - ptrEqual[q] * R;
+		}
+	}
+	dptrSolvedMatrix = dptrMatrix;
+	for (int q = N - 1; q >= 0; q--) {
+		R = ptrEqual[q];
+		for (int j = q + 1; j < N; j++) {
+			R -= dptrMatrix[q][j] * x[j];
+		}
+		x[q] = R;
+	}
+	return x;
+}
+
+double gaussDet(double ** dptrMatrix) {
+	double tmp, det = 1;
+	for (int k = 0; k < N - 1; k++) {
+		for (int i = k + 1; i < N; i++) {
+			tmp = -dptrMatrix[i][k] / dptrMatrix[k][k];
+			for (int j = 0; j < N; j++) {
+				dptrMatrix[i][j] += dptrMatrix[k][j] * tmp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		det *= dptrMatrix[i][i];
+	}
+	return det;
+}
+
+double ** gaussReverse(double ** dptrMatrix) {
+	double ** res = new double * [N];
+	double * y = new double[N];
+	double * gauss;
+	int i, j, k;
+	for (int i = 0; i < N; i++) {
+		res[i] = new double[N];
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i == j) {
+				y[j] = 1;
+			}
+			else {
+				y[j] = 0;
+			}
+		}
+		gauss = gaussSolve(copyMatrix(dptrMatrix), y);
+		for (int k = 0; k < N; k++) {
+			res[k][i] = gauss[k];
+		}
+	}
+	return res;
+}
+
+
---
+++ _(C++)CM_Lab2/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include "gausslib.h"
+
+std::string * SLAU2String(double ** dptrMatrix, double * ptrEqual) {
+	std::string * stringify = new std::string[N];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			stringify[i] += dptrMatrix[i][j] >= 0 ? ((j > 0 ? " + " : "")
+				+ std::to_string(dptrMatrix[i][j])) : (" - "
+			    + std::to_string(dptrMatrix[i][j] * -1));
+			stringify[i] += "x" + std::to_string(j + 1);;
+		}
+		stringify[i] += " = " + std::to_string(ptrEqual[i]);
+	}
+	return stringify;
+}
+
+double ** dptrMatrix = new double* [N] {
+	new double[N] {-1, 0.28, -0.17, 0.06},
+	new double[N] {0.52, -1, 0.12, 0.17},
+	new double[N] {0.17, -0.18, -0.79, 0},
+	new double[N] {0.11, 0.22, 0.03, -0.95}
+};
+double * ptrEqual = new double[N] {
+	-21, 117, 0.81, -0.72
+};
+
+int main(void) {
+	setlocale(LC_ALL, "");
+	std::cout << "СЛАУ:" << std::endl;
+	for (int i = 0; i < N; i++) {
+		std::cout << SLAU2String(copyMatrix(dptrMatrix), copyVector(ptrEqual))[i] << std::endl;
+	}
+	std::cout << std::endl << "Корни:" << std::endl;
+	for (int i = 0; i < N; i++) {
+		std::cout << "x" << (i + 1) << " == " << gaussSolve(copyMatrix(dptrMatrix), copyVector(ptrEqual))[i] << std::endl;
+	}
+	std::cout << std::endl << "Определитель: " << gaussDet(copyMatrix(dptrMatrix)) << std::endl;
+	std::cout << std::endl << "Обратная матрица: ";
+	for (int i = 0; i < N; i++) {
+		std::cout << std::endl;
+		for (int j = 0; j < N; j++) {
+			std::cout << gaussReverse(copyMatrix(dptrMatrix))[i][j] << "|";
+		}
+	}
+	_getch();
+	return 0;
+}
